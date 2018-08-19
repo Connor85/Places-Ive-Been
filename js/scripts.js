7352abe2b34789188dfce174868b9c96e1554ac0
cm <connormccarthy8@gmail.com>
2018-08-19 13:45:45 -0700

    add files and inital js

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+// business logic
+function Destination(location, landmark) {
+  this.locationName = Location;
+  this.landmarkName = Landmark;
+}
+
+// user logic
+
+$(document).ready(function () {
+  $("form#travel-guide").submit(function (event) {
+    event.prefentDefault();
+
+    var inputtedLoction = $("input#new-travel-Location");
+    var inputtedLocation = $("input#new-landmark");
+    var inputtedTime = $("input#new-time-of-year");
+    var inputtedNotes = $("input#new-notes");
+
+  });
+});
