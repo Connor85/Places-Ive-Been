99d966ed4ccf963105fbb042db3bfcf0246f15c0
cm <connormccarthy8@gmail.com>
2018-08-19 16:41:53 -0700

    add code and working out bugs

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,39 @@
 // business logic
-function Destination(location, landmark) {
-  this.locationName = Location;
-  this.landmarkName = Landmark;
+function Location(location, landmark) {
+  this.locationName = location;
+  this.landmarkName = landmark;
+  this.timeYear = timeOfYear;
+  this.notesTrip = notes;
 }
 
 // user logic
 
 $(document).ready(function () {
   $("form#travel-guide").submit(function (event) {
-    event.prefentDefault();
+    event.preventDefault();
 
-    var inputtedLoction = $("input#new-travel-Location");
+    var inputtedLoction = $("input#new-travel-location");
     var inputtedLocation = $("input#new-landmark");
     var inputtedTime = $("input#new-time-of-year");
     var inputtedNotes = $("input#new-notes");
+
+    var newLocation = new Location(inputtedLoction, inputtedLocation, inputtedTime, inputtedNotes);
+
+    $("ul#location").append("<li>" + newLocation.location + "</li>")
+
+    $("#travelLog").last().click(function () {
+      $("#destination").show();
+      $("#destination h2").text("newLocation.locationName");
+      $("#destination").text("newLocation.locationName");
+      $("#destination").text("newLocation.landmarkName");
+      $("#destination").text("newLocation.timeOfYear");
+      $("#destination").text("newLocation.notesTrip");
+    });
+
+    $("input#new-travel-location").val("");
+    $("input#new-landmark").val("");
+    $("input#new-time-of-year").val("");
+    $("input#new-notes").val("");
 
   });
 });
